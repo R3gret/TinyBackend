75fb74174ed267bdb8540abc465ef93fbca3c2a6
R3gret <biluganpierepaolo@gmail.com>
2025-04-30 05:10:02 +0800

    second commit

--- routes/add_activity.js
+++ routes/add_activity.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 
-router.post('/add_activity', async (req, res) => {
+router.post('/', async (req, res) => {
   const { date, activities } = req.body;
   let connection;
 
