e44f2346d164ac0fd6f308c2e16392ae4e7d3228
R3gret <biluganpierepaolo@gmail.com>
2025-11-11 20:33:03 +0800

    Just adding local port

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3001;
 // Security middlewares
 app.use(helmet());
 app.use(cors({
-  origin: [process.env.FRONTEND_URL || 'http://localhost:3000', 'http://localhost:5173'],
+  origin: [process.env.FRONTEND_URL || 'http://localhost:3000', 'http://localhost:5173', 'http://localhost:5174'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
